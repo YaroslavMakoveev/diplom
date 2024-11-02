185673f3834155194285c33b595f39f53cb713fc
Yarik <you@example.com>
2024-11-02 20:44:14 +0300

    PIZDA

---
+++ server/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const start = () => {
+    try {
+        app.listen(PORT, () => {console.log(`server is started on port ${PORT}`)})
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+start();
